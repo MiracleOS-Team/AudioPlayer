6ff5432dd768d8b5b74a04a9e90ced175bbe7d42
wqreloxzz <matvei.kobrusev22@gmail.com>
2025-01-25 22:27:09 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# AudioPlayer
-MiracleOS VideoPlayer utility
+# Cutefish Video Player
+
+An open source video player built with Qt/QML and libmpv.
+
+# Build and Install
+
+```
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr ..
+make
+sudo make install
+```
+
+# License
+
+This project has been licensed by GPLv3.
---
+++ translations/zh_CN.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>PlayList</name>
+    <message>
+        <location filename="../qml/PlayList.qml" line="32"/>
+        <source>Playlist is empty</source>
+        <translation>плейлист пустой</translation>
+    </message>
+</context>
+<context>
+    <name>PlayListItem</name>
+    <message>
+        <location filename="../qml/PlayListItem.qml" line="36"/>
+        <source>Play</source>
+        <translation>Играть</translation>
+    </message>
+    <message>
+        <location filename="../qml/PlayListItem.qml" line="41"/>
+        <source>Remove from list</source>
+        <translation>Удалить из списка</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="47"/>
+        <source>Video Player</source>
+        <translation>Видео плеер</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="94"/>
+        <source>Open</source>
+        <translation>Открыть</translation>
+    </message>
+</context>
+</TS>
